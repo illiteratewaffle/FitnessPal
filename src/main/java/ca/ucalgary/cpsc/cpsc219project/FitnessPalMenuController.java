cf1fbaa789703cb036ff0c8b6e7ccd13b0b32e97
BuildTools <unconfigured@null.spigotmc.org>
2024-04-01 15:37:58 -0600

    Link Menu and Logs together

--- src/main/java/ca/ucalgary/cpsc/cpsc219project/FitnessPalMenuController.java
+++ src/main/java/ca/ucalgary/cpsc/cpsc219project/FitnessPalMenuController.java
@@ -4,17 +4,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class FitnessPalMenuController extends Application {
 
-
-
     @FXML
     protected void onAddWorkoutClick() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(FitnessPalApplication.class.getResource("FitnessPalLogs.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
 
+            stage.setTitle("Add Workout");
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -23,14 +33,4 @@ public class FitnessPalMenuController extends Application {
         System.out.println("open view workouts menu");
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(FitnessPalApplication.class.getResource("FitnessPalLogs.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 480, 240);
-
-        stage.setTitle("Add Workout");
-        stage.setScene(scene);
-
-        stage.show();
-    }
 }
