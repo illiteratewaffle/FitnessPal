633f0c9207ed5a7128cb660981ed4319ec030430
Muhammad HASHAM <muhammad.hasham@ucalgary.ca>
2024-04-04 19:52:00 +0000

    Update file FilesTest.java

--- src/test/java/ca/ucalgary/cpsc/cpsc219project/FilesTest.java
+++ src/test/java/ca/ucalgary/cpsc/cpsc219project/FilesTest.java
@@ -10,9 +10,11 @@ class FilesTest {
     @Test
     void testOpenFile_correctFile() {
         Files files = new Files();
+        //Defines test username
         String testUsername = "workouts"; // Assuming 'workouts' is the username
+        //Defines expected file path
         File expected = new File("src/" + testUsername + ".txt");
-
+        //calls openFile with test username and stores result
         File actual = files.openFile(testUsername);
 
         assertEquals(expected.getAbsolutePath(), actual.getAbsolutePath(), "The opened file path should match the expected.");
